a38b76e035011d19bab35d0b867cb0e502aad504
James Robertson <Jor4291@gmail.com>
2022-01-19 18:01:08 -0600

    successfully pulling data from the api

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios'
 import { API_KEY} from './constants.js'
+import { render } from "@testing-library/react";
+
+
+
 
 
 
 function App() {
- 
+  const [image, setImage] = useState();
  
  useEffect(()=> {
   axios.get(`${API_KEY}`)
+  .then(res =>{
+    console.log(res.data)
+  }).catch(err => console.log(err))
  }, [])
  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1> Nasa's Photo of the Day!</h1>
+      {/* <img src  /> */}
+        {/* // Pass in img props and import it from image.js */}
+      
     </div>
   );
 }
